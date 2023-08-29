c86542445a6c1546eb0a94af84760a571bf86a4f
nay726 <57126026+nay726@users.noreply.github.com>
2023-08-29 02:51:02 -0700

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,34 @@
+bangla=int(input("enter marks:"))
+eng=int(input("enter marks:"))
+math=int(input("enter marks:"))
+religion=int(input("enter marks:"))
+science=int(input("enter marks:"))
+ict=int(input("enter marks:"))
+
+total_marks=600
+
+got_marks=bangla+eng+math+religion+science+ict
+print(got_marks)
+avg_mark=got_marks/6
+if bangla and eng and math and religion and science and ict <=32:
+    print("You are Failed")
+else:
+
+    if avg_mark>=80 and avg_mark<=100:
+        print("you got A+")
+    elif avg_mark>=70and avg_mark<=79:    
+        print("You got A")
+    elif avg_mark>=60and avg_mark<=69:    
+        print("You got A-")
+    elif avg_mark>=50and avg_mark<=59:    
+        print("You got B")
+    elif avg_mark>=40and avg_mark<=49:    
+        print("You got C")
+    elif avg_mark>=33and avg_mark<=39:    
+        print("You got D")
+    else:
+        print("Failed")    
+
+    
+
+
